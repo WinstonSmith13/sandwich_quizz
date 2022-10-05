b9a255a0151bead6a02c9be38149f9b75097abc9
Medy <krouk.medy@gmail.com>
2022-10-05 17:26:11 +0200

    Commit de fin de journée essai de changer les fiches.

--- Views/Templates/fiche.tmpl.php
+++ Views/Templates/fiche.tmpl.php
@@ -1,32 +1,34 @@
 
 <!--<div id="affichage_score"> Taux de réussite: 0% </div>-->
-<div id="container_fiche">
+<div id="container_fiche ">
 
-
+    <div id="affichage_score"> Taux de réussite: 0% </div>
 
 <?php
 
 
-foreach ($data as $fiche) {
+foreach ($data as $fiche) {$i = 0
+
+
 ?>
-    <div id="container_questions" class="d-none">
+    <div id="container_questions" data-id="<?= $i?>" class="d-none">
         <div class="question">
                 <div class="affichage_question"> <?= $fiche->question; ?> ?</div>
                     <div class="answers">
-                        <?php foreach ($fiche->answers as $answer) { ?>
+                        <?php foreach ($fiche->answers as $answer) {?>
 
-                            <div class="answer">
+                            <div class="answer" data-id="<?= $answer['id']?>">
                                 <?= $answer['answer'] ?>
                             </div>
 
-                        <?php } ?>
+                        <?php  } ?>
 
                     </div>
                     <div class="question_feedback"> </div>
         </div>
 
     <?php
-} ?>
+    $i++; } ?>
 
 
     </div>
--- public/Js/quizz_fiche.js
+++ public/Js/quizz_fiche.js
@@ -4,26 +4,64 @@ window.onload = () => {
     /* Maintenant que l'on a les informations du site il faut pouvoir savoir où l'on clique et comparer */
 
 
-    if (data.success === true) {
-        for (const element of data.content) {
-            for (const answers of element.answers) {
-                console.log(answers)
-            }
-        }
-    }
-
-    const questionsAffichage = document.querySelectorAll('#container_questions');
+ if (data.success === true) {
+     let i = 0;
+    let questionsAffichage = document.querySelector('[data-id = "' + i +'"]');
     const answerButton = document.querySelectorAll('.answer');
-
-    debut_jeu()
-    function debut_jeu(){
-        answerButton.forEach(answer)=>
+    const selectionScore = document.querySelector('#affichage_score');
+    const selectionQuestionCard = document.querySelector('.question')
 
 
+     questionsAffichage.classList.remove("d-none")
+
+
+    function nextQuestion(){
+
+        /*for (let i=0; i < 6; i++) { }*/
+
+
+            questionsAffichage.classList.add('d-none');
+            i++;
         questionsAffichage.classList.remove('d-none');
+            console.log(questionsAffichage);
+            /*questionsAffichage.classList.remove("d-none");*/
+
 
     }
 
 
-}
+
+
+
+
+
+let score = 0;
+
+
+
+    answerButton.forEach(answer => {
+        answer.addEventListener('click', () =>{
+            for (const element of data.content) {
+                for (const answers of element.answers) {
+
+                    id_answer= answers.id
+                    answer_check= answers.answer_check
+                    if( answer.dataset.id == id_answer && answer_check == 1 ) {
+                        score++
+                        let affichageScore = `<div class="affichage_score">Taux de réussite: ${Math.round(score*100/6)} %</div>`
+                        selectionScore.innerHTML = affichageScore;
+
+                        nextQuestion()
+
+
+                    }
+
+                }
+            }
+
+
+        })
+        })
+
+}}
 
