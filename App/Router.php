c712d3eec331d1853a76817926b363c4cd2d4b36
Medy <krouk.medy@gmail.com>
2022-10-13 16:32:49 +0200

    Update Router.php

--- App/Router.php
+++ App/Router.php
@@ -1,26 +1,18 @@
 <?php
 
-/*
- * Le routeur réceptionne des variables $_POST, $_GET ou AJAX fetch.
- * Son rôle unique sera de router l'application vers les contôleurs
- * qui  définissent la logique de l'application puis génèrerent les affichages.
- */
+
 use WINSTON\SandwichQuizz\Controllers\fiche;
 use WINSTON\SandwichQuizz\Controllers\answer;
 
 
 
-
-// On démarre le moteur de sessions PHP pour gérer les variables de $_SESSION.
+// Démarrage du moteur de sessions PHP pour gérer les variables de $_SESSION.
 session_start();
-
 
 /*
  * Gestion des appels avec POST.
  */
 if(!empty($_POST)) {
-
-
 }
 
 
@@ -51,7 +43,7 @@ if(!empty($data)) {
 
     if (isset($data->idAnswer)){
         $check = new answer();
-        echo ($check-> answerCheck($data->idAnswer));
+        echo ($check->answerCheck($data->idAnswer));
         exit();
     }
 
