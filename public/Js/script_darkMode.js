02d1a3a965b653026864a7a4add2ee6c767eb5e1
WinstonSmith13 <krouk.medy@gmail.com>
2023-01-07 16:06:19 +0100

    cleaning: Cleaning Code.

--- public/Js/script_darkMode.js
+++ public/Js/script_darkMode.js
@@ -1,50 +1,46 @@
-/**
- * Dark Mode
- */
-
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 const darkmode = document.querySelector(".text-bg-light");
 
-
 const darkModeBtn = document.querySelector(".darkModeBtn");
 
-function dark(){
-    body.classList.add("text-bg-dark");
-    header.classList.add("text-bg-dark");
-    footer.classList.add("text-bg-dark");
-    darkmode.classList.add("text-bg-dark");
-    document.cookie = "darkMode = false; SameSite=None; Secure"
-
-}
-
-function light(){
-    document.body.classList.remove("text-bg-dark");
-    header.classList.remove("text-bg-dark");
-    footer.classList.remove("text-bg-dark");
-    darkmode.classList.remove("text-bg-dark");
-    document.cookie = "darkMode = true; SameSite=None; Secure"
-
-}
-
-
-if (document.cookie.split(";").some((item) => item.includes("darkMode=false"))) {
-    dark();
-} else {
-    light();
-}
-
-
-
-darkModeBtn.addEventListener("click", () => {
-    if (!body.classList.contains("text-bg-dark")) {
-        darkModeBtn.innerHTML = "<span>Light Mode</span>";
-        dark();
-
+function setDarkMode(isDark) {
+    if (isDark) {
+        body.classList.add("text-bg-dark");
+        header.classList.add("text-bg-dark");
+        footer.classList.add("text-bg-dark");
+        darkmode.classList.add("text-bg-dark");
+        document.cookie = "darkMode=true; SameSite=None; Secure";
     } else {
-        darkModeBtn.innerHTML = "<span>Dark Mode</span>";
-
-        light();
+        body.classList.remove("text-bg-dark");
+        header.classList.remove("text-bg-dark");
+        footer.classList.remove("text-bg-dark");
+        darkmode.classList.remove("text-bg-dark");
+        document.cookie = "darkMode=false; SameSite=None; Secure";
     }
-});
+}
+
+function toggleDarkMode() {
+    const isDark = body.classList.contains("text-bg-dark");
+    setDarkMode(!isDark);
+    darkModeBtn.innerHTML = `<span>${isDark ? "Dark" : "Light"} Mode</span>`;
+}
+
+let isDarkMode = false;
+
+if (document.cookie) {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === "darkMode") {
+            isDarkMode = value === "true";
+            break;
+        }
+    }
+}
+
+setDarkMode(isDarkMode);
+darkModeBtn.innerHTML = `<span>${isDarkMode ? "Light" : "Dark"} Mode</span>`;
+
+darkModeBtn.addEventListener("click", toggleDarkMode);
--- public/Js/script_quizz.js
+++ public/Js/script_quizz.js
@@ -1,75 +1,55 @@
 window.onload = () => {
-
     //Compteur Score.
-    let score;
+    let score = 0;
     //Compteur pour affichage des fiches.
     let i = 0;
     //Compteur pour le compteur de question dans le header de la fiche.
     let counterQuestion = 1;
 
-    const answerButton = document.querySelectorAll('.answer');
+    const answerButtons = document.querySelectorAll('.answer');
     let selectionQuestion = document.querySelector('#container_questions' + i);
     let counterQuestionAff = document.querySelector('.title');
     let selectionScore = document.querySelector('.score_aff');
 
-    //Vérification de la réponse (faux ou vrai) du côté serveur.
-
-    function answer_check(idAnswer) {
-        fetch("/", {
+    // Vérification de la réponse (faux ou vrai) du côté serveur.
+    function answerCheck(idAnswer) {
+        return fetch("/", {
             method: 'POST',
-            body: JSON.stringify({idAnswer: idAnswer})
+            body: JSON.stringify({ idAnswer: idAnswer })
         })
             .then((response) => {
                 return response.json();
-            }).then(data => {
-            //Récupération de la valeur de la table "answer_check" dans la base de donnée (soit 0 ou 1) et ajout dans
-            //la variable score.
+            })
+            .then(data => {
+                // Récupération de la valeur de la table "answer_check" dans la base de donnée (soit 0 ou 1) et ajout dans la variable score.
+                score += data;
+                console.log(score);
 
-                score =  data ;
-                console.log(score)
-
-
-            //score += data.content;
-
-            //Affiche du score à la suite de la réponse.
-            selectionScore.innerHTML = `<div class="score_aff"> ${Math.round(score * 100 / 6)} %</div>`;
-
-        })
-
-
+                // Affiche du score à la suite de la réponse.
+                selectionScore.innerHTML = `<div class="score_aff"> ${Math.round(score * 100 / 6)} %</div>`;
+            });
     }
 
-    //Affichage de la première question.
+    // Affichage de la première question.
     selectionQuestion.classList.remove('d-none');
 
-
-    //Ajout du dynamisme.
-    answerButton.forEach(answer => {
+    // Ajout du dynamisme.
+    answerButtons.forEach(answer => {
         answer.addEventListener('click', () => {
-
             if (i <= 4) {
-                //Vérification de la réponse.
-                answer_check(answer.id);
-                //Passage à la fiche de question suivante.
+                // Vérification de la réponse.
+                answerCheck(answer.id);
+                // Passage à la fiche de question suivante.
                 document.querySelector('#container_questions' + i).classList.add("d-none");
                 i++;
-
                 counterQuestion++;
                 counterQuestionAff.innerHTML = `<span>${counterQuestion} / 6</span>`;
                 document.querySelector('#container_questions' + i).classList.remove('d-none');
             } else {
-                answer_check(answer.id);
-
-                //Ajout de la class "d-none" pour cacher le header des fiches.
-                /*document.querySelector('#container_questions' + i).classList.add('d-none');
-                document.querySelector('.header_fiche').classList.add('d-none');
-                document.querySelector('.title').classList.add('d-none');
-                document.querySelector('#result').classList.remove('d-none');*/
+                answerCheck(answer.id);
+                // Redirection vers la page des résultats.
                 window.location.replace("/?score");
-
             }
-        })
-    })
+        });
+    });
 }
-
-
